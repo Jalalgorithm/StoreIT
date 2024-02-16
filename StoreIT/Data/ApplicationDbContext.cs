c92de1dba707429f46716081d85dec8ea6dd1ecd
Jalalgorithm <temitomzi@gmail.com>
2024-02-16 23:22:46 +0100

    Adding DbContext

---
+++ StoreIT/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StoreIT.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+            
+        }
+    }
+}
--- StoreIT/Program.cs
+++ StoreIT/Program.cs
@@ -1,11 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using StoreIT.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+         migration => migration.MigrationsAssembly("StoreIT"));
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
