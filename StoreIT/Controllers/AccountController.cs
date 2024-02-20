dab802aa57de17d6d44a7fdc9daae84e3110cb3f
Jalalgorithm <temitomzi@gmail.com>
2024-02-20 21:29:25 +0100

    Adding User to Database

--- StoreIT/Controllers/AccountController.cs
+++ StoreIT/Controllers/AccountController.cs
@@ -18,6 +18,8 @@ namespace StoreIT.Controllers
         {
             _configuration = configuration;
         }
+
+
         private string CreateJwt (User user)
         {
             List<Claim> claims = new List<Claim>()
--- StoreIT/Data/ApplicationDbContext.cs
+++ StoreIT/Data/ApplicationDbContext.cs
@@ -11,5 +11,6 @@ namespace StoreIT.Data
             
         }
         public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
